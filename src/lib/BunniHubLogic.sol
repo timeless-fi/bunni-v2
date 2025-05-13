224e133906e56c0c67199a002a67d9baaf0e5fe0
ZeframLou <zeframlou@gmail.com>
2025-05-12 18:48:59 -0700

    gas: simplify BunniHubLogic::queueWithdraw()

--- src/lib/BunniHubLogic.sol
+++ src/lib/BunniHubLogic.sol
@@ -434,13 +434,11 @@ library BunniHubLogic {
             if (params.shares == 0) revert BunniHub__ZeroInput();
             s.queuedWithdrawals[id][msgSender] =
                 QueuedWithdrawal({shareAmount: params.shares, unlockTimestamp: newUnlockTimestamp});
-        }
 
-        /// -----------------------------------------------------------------------
-        /// External calls
-        /// -----------------------------------------------------------------------
+            /// -----------------------------------------------------------------------
+            /// External calls
+            /// -----------------------------------------------------------------------
 
-        if (queued.shareAmount == 0) {
             // transfer shares from msgSender to address(this)
             bunniToken.transferFrom(msgSender, address(this), params.shares);
         }
